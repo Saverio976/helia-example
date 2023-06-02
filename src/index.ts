d149c98c717d9c810b91d7697023c8bab1658c9c
Xavier Mitault <xavier.mitault@epitech.eu>
2023-06-02 17:29:53 +0200

    Add libp2p and datastore

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { createHelia } from 'helia';
 import { FsBlockstore } from 'blockstore-fs';
+import { FsDatastore } from 'datastore-fs';
 import { strings } from '@helia/strings';
 import { json } from '@helia/json';
 import { CID } from 'multiformats/cid'
+import { createLibp2p } from 'libp2p';
 
-import config from './config.js';
+import {config, configp2p} from './config.js';
 
 const printHelp = () => {
     console.log("USAGE:");
@@ -22,15 +24,28 @@ if (process.argv[2] === 'help') {
     process.exit(0);
 }
 
-async function main() {
-    // Comment stocker les info
+async function createNode() {
+    // Comment stocker les block
     // blockstore-fs -> file system
     // blockstore-level -> leveldb (database)
     // blockstore-core -> in memory
     // plus d'info: https://github.com/ipfs-examples/helia-101/blob/1c6a35364071512b86b48bb48ca6fa301f2dab1b/201-storage.js#L7-L12
-    const blockstore = new FsBlockstore(config.dbDir);
+    const blockstore = new FsBlockstore(config.dbDir, {createIfMissing: true});
 
-    const helia = await createHelia({blockstore});
+    const datastore = new FsDatastore(config.dbDir, {createIfMissing: true});
+    // créer le dossier si il existe pas (fait une erreur sinon, je sais pas pourquoi)
+    await datastore.open();
+    await datastore.close();
+
+    const libp2p = await createLibp2p(configp2p(datastore));
+
+    const helia = await createHelia({blockstore, datastore, libp2p});
+
+    return helia;
+}
+
+async function main() {
+    const helia = await createNode();
 
     if (process.argv[3] === 'add') {
         let cid: CID;
